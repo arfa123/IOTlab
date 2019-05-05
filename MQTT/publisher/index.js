d11458444aa8104700ccc871505832354aca58c6
arfa123 <aphahmed@yahoo.com>
2019-05-05 21:10:37 +0500

    MQTT programs moved

--- MQTT/publisher/index.js
+++
@@ -1,8 +0,0 @@
-var mqtt = require('mqtt');
-var client  = mqtt.connect('mqtt://<mqtt-server-ip>');
-client.on('connect', function () {
-	setInterval(function() {
-	client.publish('onoff', 'Turn on the light');
-	console.log('Message Sent');
-	}, 5000);
-});
--- MQTT/subscriber/index.js
+++
@@ -1,9 +0,0 @@
-var mqtt = require('mqtt')
-var client = mqtt.connect('mqtt://<mqtt server ip>')
-client.on('connect', function () {
-    client.subscribe('#');
-});
-client.on('message', function (topic, message) {
-	context = message.toString();
-	console.log(context)
-});
